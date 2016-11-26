5a47f1f0a072c8df58689e14190aa1804038edc7
jackalcooper <jackalcooper@gmail.com>
2016-11-26 22:42:23 +0800

    Update 1-standalone.elm

--- examples/1-standalone.elm
+++ examples/1-standalone.elm
@@ -1,3 +1,5 @@
+module Main exposing (..)
+
 import Date exposing (Date, Month(..))
 import Date.Extra as Date
 import DateSelector
@@ -7,44 +9,56 @@ import String
 
 main : Program Never Model Date
 main =
-  Html.beginnerProgram
-    { model =
-        Model
-          (Date.fromCalendarDate 2011 Mar 15)
-          (Date.fromCalendarDate 2017 Sep 15)
-          (Date.fromParts 2016 Sep 15 9 0 0 0)
-    , view = view
-    , update = update
-    }
+    Html.beginnerProgram
+        { model =
+            Model
+                (Date.fromCalendarDate 2011 Mar 15)
+                (Date.fromCalendarDate 2017 Sep 15)
+                (Date.fromParts 2016 Sep 15 9 0 0 0)
+        , view = view
+        , update = update
+        }
+
 
 
 -- model
 
+
 type alias Model =
-  { minimum : Date
-  , maximum : Date
-  , selected : Date
-  }
+    { minimum : Date
+    , maximum : Date
+    , selected : Date
+    }
+
 
 
 -- update
 
+
 update : Date -> Model -> Model
 update date model =
-  { model | selected = date }
+    { model | selected = date }
+
 
 
 -- view
 
+
 view : Model -> Html Date
 view { minimum, maximum, selected } =
-  div []
-    [ Html.node "style" []
-        [ text <| String.join " "
-            [ "@import url(./examples.css);"
-            , "@import url(./date-selector.css);"
+    let
+        markedDates =
+            [ (Date.fromCalendarDate 2016 Sep 16) ]
+    in
+        div []
+            [ Html.node "style"
+                []
+                [ text <|
+                    String.join " "
+                        [ "@import url(./examples.css);"
+                        , "@import url(./date-selector.css);"
+                        ]
+                ]
+            , h1 [] [ text <| Date.toFormattedString "EEE MMM d, yyyy" selected ]
+            , DateSelector.viewWithMarked minimum maximum (Just selected) markedDates
             ]
-        ]
-    , h1 [] [ text <| Date.toFormattedString "EEE MMM d, yyyy" selected ]
-    , DateSelector.view minimum maximum (Just selected)
-    ]
